5fab5d9af751a932333bd01c1ba2e278e2a5aab7
Matthew Strait <strait@physics.umn.edu>
2018-02-12 11:38:30 -0600

    runset.sh: Make it work instead of not.
    Now runset.sh writes a specified time into a temporary
    fcl file and runs everything with that.  It also saves
    logs and concatenates all the resulting histograms for you.

--- runset.sh
+++ runset.sh
@@ -5,14 +5,54 @@ if ! [ $1 ]; then
   echo \(That is, the part of the name of the fcl between ligojob_ and .fcl\)
   exit 1
 fi
-
 type=$1
-
 shift
 
+if ! [ $1 ]; then
+  echo specify the unix time of the event
+  exit 1
+fi
+unixtime=$1
+shift
+
+export TZ=UTC
+rfctime=$(date "+%Y-%m-%dT%H:%M:%SZ" -d @$unixtime)
+
+fcl=ligojob_$type.$rfctime.fcl
+
+if ! [ -e $SRT_PRIVATE_CONTEXT/job/ligojob_$type.fcl ]; then
+  echo Analysis type $type not supported
+  exit 1
+fi
+
+cat $SRT_PRIVATE_CONTEXT/job/ligojob_$type.fcl | \
+  sed "/this_here_ligo: @local/a this_here_ligo.GWEventTime: \"$rfctime\"" > $fcl
+
+# Need to fail if nova fails in 'nova | tee'
+set -o pipefail
+
+if ! [ $1 ]; then
+  echo You did not specify any files to processes.  Success\!
+  exit 0
+fi
+
 for f in $@; do
+  if ! [ -e "$f" ]; then
+    echo $f does not exist
+    exit 1
+  fi
+
   base=$(basename $f .artdaq.root)
-  nova -c ligojob_$type.fcl $f \
-       -o $base.ligo.root \
-       -T $base.hists.root
+  reco=$base.ligo.root
+  log=$base.ligo.log
+  hist=$base.hists.root
+  hists="$hists $hist"
+  if ! nova $f -c $fcl -o $reco -T $hist 2> /dev/stdout | tee $log; then
+    exit 1
+  fi
 done
+
+out=$type.ligohists.root
+
+hadd -f $out $hists
+echo Full histograms in $out
