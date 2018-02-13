772ce34f33aecc4bad3197d685b6787b86cf35b2
Matthew Strait <strait@physics.umn.edu>
2018-02-13 08:56:58 -0600

    getfilelist.sh: Continue if def exists.
    If the SAM definition exists, skip to the part where
    we check the cache instead of quitting.
    
    Also fix spelling of "list-definitions" so that those
    commands work.

--- getfilelist.sh
+++ getfilelist.sh
@@ -10,49 +10,47 @@ else
 fi
 
 def=strait-ligo-coincidence-artdaq-$t
-if samweb list-definintions | grep -qE "^$def$"; then
+if samweb list-definitions | grep -qE "^$def$"; then
   echo SAM definition $def already exists for $t
-  echo Maybe delete it before trying to make another?
-  exit 1
-fi
+else
+  if ! [ -e allfiles.$t ]; then
+    # I want a 1000 second window, but also add a 50 second buffer on each
+    # end because I have observed, at least with DDEnergy files, that the
+    # events are out of order on the scale of 10 seconds and the metadata
+    # variables don't seem to know that.
+    #
+    # Fantastically slow
+    echo Asking SAM for a list of files. This typically takes a
+    echo stubstantial fraction of the age of the universe to complete.
+    samweb list-files \
+           'Online.SubRunEndTime   > '$((t-550))\
+      ' and Online.SubRunStartTime < '$((t+550)) > allfiles.$t
+  fi
 
-if ! [ -e allfiles.$t ]; then
-  # I want a 1000 second window, but also add a 50 second buffer on each
-  # end because I have observed, at least with DDEnergy files, that the
-  # events are out of order on the scale of 10 seconds and the metadata
-  # variables don't seem to know that.
-  #
-  # Fantastically slow
-  echo Asking SAM for a list of files. This typically takes a
-  echo stubstantial fraction of the age of the universe to complete.
-  samweb list-files \
-         'Online.SubRunEndTime   > '$((t-550))\
-    ' and Online.SubRunStartTime < '$((t+550)) > allfiles.$t
-fi
+  for trigger in \
+      'fardet.*_t02_.*data.artdaq' \
+      'neardet.*_bnb_.*data.artdaq' \
+      'fardet.*_ddenergy_.*data.artdaq' \
+      'fardet.*_ddupmu_.*data.artdaq' \
+      'fardet.*_ddnumu_.*data.artdaq' \
+      'fardet.*_ddfastmono_.*data.artdaq' \
+      'fardet.*_ddslowmono_.*data.artdaq' \
+      'fardet.*_ddcontained_.*data.artdaq' \
+      'neardet.*_ddactivity1_.*data.artdaq' \
+      ; do
+    cat allfiles.$t | grep -E "$trigger"
+    echo
+  done | tee selectedfiles.$t
 
-for trigger in \
-    'fardet.*_t02_.*data.artdaq' \
-    'neardet.*_bnb_.*data.artdaq' \
-    'fardet.*_ddenergy_.*data.artdaq' \
-    'fardet.*_ddupmu_.*data.artdaq' \
-    'fardet.*_ddnumu_.*data.artdaq' \
-    'fardet.*_ddfastmono_.*data.artdaq' \
-    'fardet.*_ddslowmono_.*data.artdaq' \
-    'fardet.*_ddcontained_.*data.artdaq' \
-    'neardet.*_ddactivity1_.*data.artdaq' \
-    ; do
-  cat allfiles.$t | grep -E "$trigger"
-  echo
-done | tee selectedfiles.$t
+  samweb create-definition $def \
+    "$(for f in $(cat selectedfiles.$t | grep -v ^$); do
+         printf "%s %s or " file_name $(basename $f);
+       done | sed 's/ or $//')"
 
-samweb create-definition $def \
-  "$(for f in $(cat selectedfiles.$t | grep -v ^$); do
-       printf "%s %s or " file_name $(basename $f);
-     done | sed 's/ or $//')"
-
-if ! samweb list-definintions | grep -qE "^$def$"; then
-  echo Failed to make a SAM definition
-  exit 1
+  if ! samweb list-definitions | grep -qE "^$def$"; then
+    echo Failed to make a SAM definition
+    exit 1
+  fi
 fi
 
 cachedpercent=$(cache_state.py -d $def | tee /dev/stderr | \
