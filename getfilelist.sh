a258f4702bcd1d496c49dc126d018117d777f952
Matthew Strait <strait@physics.umn.edu>
2018-02-13 11:21:18 -0600

    getfilelist.sh: Separate triggers.
    The lists of files are too long to put on the command line
    if they include all triggers, so make a separate SAM definition
    for each trigger.  Maybe this is the right thing to do anyway?

--- getfilelist.sh
+++ getfilelist.sh
@@ -9,9 +9,49 @@ else
   exit 1
 fi
 
-def=strait-ligo-coincidence-artdaq-$t
-if samweb list-definitions | grep -qE "^$def$"; then
-  echo SAM definition $def already exists for $t
+filepatterns=(
+  'fardet.*_t02_.*data.artdaq'
+  'neardet.*_bnb_.*data.artdaq'
+  'fardet.*_ddenergy_.*data.artdaq'
+  'fardet.*_ddupmu_.*data.artdaq'
+  'fardet.*_ddnumu_.*data.artdaq'
+  'fardet.*_ddfastmono_.*data.artdaq'
+  'fardet.*_ddslowmono_.*data.artdaq'
+  'fardet.*_ddcontained_.*data.artdaq'
+  'neardet.*_ddactivity1_.*data.artdaq'
+)
+
+triggers=(
+  'fardet-t02'
+  'neardet-bnb'
+  'fardet-ddenergy'
+  'fardet-ddupmu'
+  'fardet-ddnumu'
+  'fardet-ddfastmono'
+  'fardet-ddslowmono'
+  'fardet-ddcontained'
+  'neardet-ddactivity1'
+)
+
+defbase=strait-ligo-coincidence-artdaq-$t
+
+havealldefs()
+{
+  need=0
+  for i in {0..8}; do
+    def=$defbase-${triggers[i]}
+    if samweb list-definitions | grep -qE "^$def$"; then
+      echo SAM definition $def already exists for $t
+    else
+      need=1
+      echo No SAM definition yet for $def
+    fi
+  done
+  return $need
+}
+
+if havealldefs; then
+  echo Have all SAM definitions already
 else
   if ! [ -e allfiles.$t ]; then
     # I want a 1000 second window, but also add a 50 second buffer on each
@@ -27,36 +67,37 @@ else
       ' and Online.SubRunStartTime < '$((t+550)) > allfiles.$t
   fi
 
-  for trigger in \
-      'fardet.*_t02_.*data.artdaq' \
-      'neardet.*_bnb_.*data.artdaq' \
-      'fardet.*_ddenergy_.*data.artdaq' \
-      'fardet.*_ddupmu_.*data.artdaq' \
-      'fardet.*_ddnumu_.*data.artdaq' \
-      'fardet.*_ddfastmono_.*data.artdaq' \
-      'fardet.*_ddslowmono_.*data.artdaq' \
-      'fardet.*_ddcontained_.*data.artdaq' \
-      'neardet.*_ddactivity1_.*data.artdaq' \
-      ; do
-    cat allfiles.$t | grep -E "$trigger"
+  for i in {0..8}; do
+    cat allfiles.$t | grep -E "${filepatterns[i]}"
     echo
   done | tee selectedfiles.$t
 
-  samweb create-definition $def \
-    "$(for f in $(cat selectedfiles.$t | grep -v ^$); do
-         printf "%s %s or " file_name $(basename $f);
-       done | sed 's/ or $//')"
+  for i in {0..8}; do
+    def=$defbase-${triggers[i]}
+    if cat selectedfiles.$t | grep -q ${filepatterns[i]}; then
+      samweb create-definition $def \
+        "$(for f in $(cat selectedfiles.$t | grep ${filepatterns[i]}); do
+             printf "%s %s or " file_name $(basename $f);
+           done | sed 's/ or $//')"
 
+      if ! samweb list-definitions | grep -qE "^$def$"; then
+        echo Failed to make a SAM definition
+        exit 1
+      fi
+    fi
+  done
+fi
+
+for i in {0..8}; do
+  def=$defbase-${triggers[i]}
   if ! samweb list-definitions | grep -qE "^$def$"; then
-    echo Failed to make a SAM definition
-    exit 1
+    continue
   fi
-fi
+  cachedpercent=$(cache_state.py -d $def | tee /dev/stderr | \
+    awk '/Cached:/{split($3, n, "("); print n[2]*1;}')
 
-cachedpercent=$(cache_state.py -d $def | tee /dev/stderr | \
-  awk '/Cached:/{split($3, n, "("); print n[2]*1;}')
-
-if [ "$cachedpercent" -lt 100 ]; then
-  echo Not all files are cached.  Caching...
-  samweb prestage-dataset --defname=$def --parallel 4
-fi
+  if [ "$cachedpercent" -lt 100 ]; then
+    echo Not all files are cached.  Caching...
+    samweb prestage-dataset --defname=$def --parallel 4
+  fi
+done
