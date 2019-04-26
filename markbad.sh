72a0f06ed637270bedb2f3d2eb04dc8c35fb0185
Matthew Strait <strait@physics.umn.edu>
2019-04-26 08:29:48 -0500

    Avoid rming everything in markbad.sh...

--- markbad.sh
+++ markbad.sh
@@ -4,6 +4,11 @@
 
 name=$(printf "$1" | sed 's@/$@@')
 
+if ! [ -d $outhistdir/$name ]; then
+  echo No hist directory for $name, doing nothing
+  exit 1
+fi
+
 touch $outhistdir/$name/bad
 
 rm -fv $outhadddir/${name}*
