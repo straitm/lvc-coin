fbcd37a2b4ab5724e380f332e367867fa0e97ddf
Matthew Strait <mstrait@fnal.gov>
2019-05-30 08:04:19 -0500

    Guess (accurately) GWNAME and such in combineall

--- combineall.sh
+++ combineall.sh
@@ -2,7 +2,26 @@
 
 if [ $1 ]; then
   GWNAME=$1
+elif [ $GWNAME ]; then
+  :
+else
+  GWNAME=$(basename "$PWD")
+  if [ $GWNAME == ${GWNAME##*-} ]; then
+    echo Please set GWNAME
+    exit 1
+  fi
+  export GWNAME=${GWNAME##*-}
+  echo I got GWNAME = $GWNAME from the PWD.  I hope that is right.
+
+  if basename "$PWD" | grep -q sideband; then
+    export SIDEBAND=1
+    echo Looks like this is a sideband sample.  I hope that is also right.
+  elif basename "$PWD" | grep -qv bg; then
+    export REALGWEVENT=1
+    echo Looks like this is a real event.  I hope that is also right.
+  fi
 fi
+
 
 . $SRT_PRIVATE_CONTEXT/ligo/env.sh
 
