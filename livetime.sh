8c84bbbd0ef39510b210906ba67940805f7a0348
Matthew Strait <mstrait@fnal.gov>
2020-08-11 22:39:53 -0500

    Update livetime.sh

--- livetime.sh
+++ livetime.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 (root -b $1 << EOF
-  ligoanalysis->cd()
-  blindlive->Integral()
+  snanalysis->cd()
+  supernovalikelive->Integral(-1, 1001)
   .q
 EOF
 ) 2> /dev/null | tail -n 1 | awk '{print $2}'
