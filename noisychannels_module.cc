ecf594b74733b7252b05149a297dab3b85217ec0
Matthew Strait <mstrait@fnal.gov>
2020-09-20 01:35:04 -0500

    noisychannels

---
+++ noisychannels_module.cc
@@ -0,0 +1,140 @@
+////////////////////////////////////////////////////////////////////////
+/// \brief The noisychannels module looks at GW coincidences.
+///
+/// Given a time window, specified as an absolute time and a delta, it
+/// searches for a set of interesting event types so that you can see
+/// if there is a spike coincident with a gravitational wave event, or,
+/// more broadly, any sort of external event you might think of. The
+/// output is a set of histograms.
+///
+/// \author M. Strait
+////////////////////////////////////////////////////////////////////////
+
+#include "art/Framework/Services/Optional/TFileService.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Core/EDAnalyzer.h"
+
+#include "DAQDataFormats/RawTriggerTime.h"
+#include "DAQDataFormats/RawEvent.h"
+#include "DAQDataFormats/RawTrigger.h"
+#include "DAQDataFormats/RawTriggerMask.h"
+#include "DAQDataFormats/RawDataBlock.h"
+#include "StandardRecord/SREnums.h"
+
+#include "RawData/FlatDAQData.h"
+#include "ChannelInfo/BadChanList.h"
+#include "RawData/RawDigit.h"
+#include "RawData/RawTrigger.h"
+
+#include "RecoBase/Track.h"
+
+#include "TH2.h"
+#include "TTree.h"
+
+#include <string>
+#include <vector>
+#include <set>
+
+#include <signal.h>
+
+#include "progress.cpp"
+
+#include "art/Framework/Core/EDProducer.h"
+#include "art/Framework/Principal/Run.h"
+#include "art/Framework/Principal/SubRun.h"
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Services/Registry/ServiceHandle.h"
+#include "art/Framework/Principal/Handle.h"
+#include "art/Framework/Core/ModuleMacros.h"
+
+#include "Calibrator/Calibrator.h"
+#include "ChannelInfo/BadChanList.h"
+#include "RawData/RawDigit.h"
+#include "RecoBase/CellHit.h"
+#include "RunHistory/service/RunHistoryService.h"
+
+// Always make it FD size and sometimes don't use the whole thing
+static const unsigned int nplane = 32*28, ncell = 12*32;
+
+static uint64_t counts[nplane][ncell];
+static TH2C * result = NULL;
+
+class noisychannels : public art::EDAnalyzer {
+  public:
+  explicit noisychannels(fhicl::ParameterSet const& pset);
+  virtual ~noisychannels() { }; // compiles, but does not run, without this
+  void analyze(const art::Event& evt);
+
+  void beginJob();
+  void endJob();
+};
+
+noisychannels::noisychannels(fhicl::ParameterSet const& pset) :
+  EDAnalyzer(pset)
+{
+}
+
+void noisychannels::beginJob()
+{
+  art::ServiceHandle<art::TFileService> t;
+
+  result = t->make<TH2C>("noisychannels", "",
+    nplane, 0, nplane, ncell, 0, ncell);
+}
+
+void noisychannels::endJob()
+{
+  // Count active channels so this works regardless of detector,
+  // and even works for partial detectors. 
+  uint64_t total = 0, activechannels = 0;
+  
+  for(unsigned int i = 0; i < nplane; i++){
+    for(unsigned int j = 0; j < ncell; j++){
+      total += counts[i][j];
+      if(counts[i][j] > 0) activechannels++;
+    }
+  }
+
+  printf("%lu channel%s seem%s active\n", activechannels,
+         activechannels == 1?"":"s",
+         activechannels == 1?"s":"");
+
+  const double mean = activechannels?total/activechannels:0;
+  const double threshold = 10*mean;
+
+  for(unsigned int i = 0; i < nplane; i++)
+    for(unsigned int j = 0; j < ncell; j++)
+      if(counts[i][j] > threshold) result->SetBinContent(i+1, j+1, 1);
+}
+
+void noisychannels::analyze(const art::Event & evt)
+{
+  {
+    static unsigned int n = 0;
+    // Start at 1 because the first event takes forever as everything
+    // is initialized.
+    if(n == 1){
+      // art provides no way of knowing how much events we will process,
+      // and its own progress indicator is of limited use.
+      printf("For progress indicator, assuming a 2000 event long readout\n");
+      initprogressindicator(2000-1, 3);
+    }
+    else if(n > 1){
+      progressindicator(n - 1);
+    }
+    n++;
+  }
+
+  // Must be called on every event to prevent SIGPIPE loops.
+  signal(SIGPIPE, SIG_DFL);
+
+  art::Handle< std::vector<rawdata::RawDigit> > hits;
+  evt.getByLabel("daq", hits);
+
+  art::ServiceHandle<cmap::CMap> map;
+
+  for(const auto & hit : *hits)
+    counts[map->GetPlane(&hit)][map->GetCell(&hit)]++;
+}
+
+DEFINE_ART_MODULE(noisychannels)
